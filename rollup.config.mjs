740d25bc4fc09f6cee6eb6df8f1dafff884b6790
Stargazer7 <1601778118@qq.com>
2023-03-13 23:40:34 +0800

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'rollup';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2';
+import json from '@rollup/plugin-json';
+import clear from 'rollup-plugin-clear'
+
+export default defineConfig({
+  input: './src/main.ts',
+  output: {
+    dir: './dist',
+    format: 'cjs',
+  },
+  plugins: [
+    typescript(),
+    resolve({
+      customResolveOptions: {
+        moduleDirectories: ['node_modules'],
+      },
+    }),
+    commonjs(),
+    json(),
+    clear({
+      // required, point out which directories should be clear.
+      targets: ['./dist'],
+      // optional, whether clear the directores when rollup recompile on --watch mode.
+      watch: true, // default: false
+  })
+  ],
+});
